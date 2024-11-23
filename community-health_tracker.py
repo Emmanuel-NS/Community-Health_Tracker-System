6d3940db6737118707aee4fbf4867aef51f138f1
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-11-23 22:16:02 +0200

    community health tracker

--- community-health_tracker.py
+++ community-health_tracker.py
@@ -18,7 +18,7 @@ def get_db_connection():
     )
 
 def clear_screen():
-    """Clear the terminal screen."""
+    # Clear the terminal screen.
     os.system('cls' if os.name == 'nt' else 'clear')
 
 class AdvancedHealthTracker:
